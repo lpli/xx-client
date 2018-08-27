e3626ac1c46081e5d30947553fd2b711d97b077e
liliping <liliping@111.com.cn>
2018-08-27 16:36:00 +0800

    ignore配置
    Change-Id: If9daa14c7a70ea1b9efc2d27876476eb2c54f8bd

---
+++ src/test/java/com/xx/client/DemoTest.java
@@ -0,0 +1,20 @@
+package com.xx.client;
+
+/**
+ * @Author liliping
+ * @Date 2018/8/27
+ **/
+public class DemoTest {
+
+    public static int test(){
+        for(int i=0;i<10;i++){
+            return 0;
+        }
+        return 1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
+}
